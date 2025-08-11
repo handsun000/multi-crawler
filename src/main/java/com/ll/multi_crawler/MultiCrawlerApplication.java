85883706777132646d8bf785d4cde046a1b68e0f
SonJinYeong <handsun000@gmail.com>
2025-08-11 14:20:09 +0900

    setting

---
+++ src/main/java/com/ll/multi_crawler/MultiCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.ll.multi_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/multi_crawler/MultiCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.multi_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
