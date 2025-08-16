8abdc666260a587c8ab25b3cefebe8b8008372df
SonJinYeong <handsun000@gmail.com>
2025-08-16 14:56:39 +0900

    work

---
+++ src/main/java/com/ll/multi_crawler/crawler/NewsCrawler.java
@@ -0,0 +1,23 @@
+package com.ll.multi_crawler.crawler;
+
+import com.ll.multi_crawler.dto.NewsDto;
+import org.jsoup.Jsoup;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NewsCrawler {
+    public List<NewsDto> crawl() {
+        List<NewsDto> newsList = new ArrayList<>();
+        try {
+
+        }
+        catch (Exception e) {
+
+        }
+        return newsList;
+    }
+}
---
+++ src/main/java/com/ll/multi_crawler/dto/NewsDto.java
@@ -0,0 +1,14 @@
+package com.ll.multi_crawler.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewsDto {
+    private String title;
+    private String url;
+    private String summary;
+}
